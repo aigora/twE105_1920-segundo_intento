76f4b8bdc4308fb51fe83c3e51e2ff9114d12ba3
David <d.almarzag@alumnos.upm.es>
2020-03-19 21:06:43 +0100

    Update CINETSIDI 1.c

--- CINETSIDI 1.c
+++ CINETSIDI 1.c
@@ -2,40 +2,55 @@
 #include <string.h>
 #include <locale.h>
 
-double main()
+int main()
 {
 	int op1;
 	int op2;
 	int num;
-	int peli;//pelicula elegida
+	int peli;
+	/*pelicula elegida*/
 	
-	setlocale(LC_ALL, "spanish");//poder escribir caracteres como la ñ y los acentos
-	
-	printf(" \t\t  -----CINETSIDI-----\n");
-	do
-	{
-	
-	printf("¿Qué desea hacer?:\n\t 1.INICIAR SESIÓN\t 2.CREAR UNA CUENTA\n");
-	scanf("%i", &op1);//decide si iniciar sesion o crear una cuenta
-	if (op1!=1&&op1!=2) //este bucle if es para decir al usuario que esta escribiendo mal el numero
-	 {printf("Operación no valida.\n Escriba 1 ó 2 en función de si desea iniar sesión o crear una cuenta.\n");
-	 }
-	}
-	while(op1!=1&&op1!=2);
 	char nombre[10];
 	char contra[10];
 	char contra1[10];
 	char contra2[10];
 	int i=0;
+	
+	char peli1[10]="SONIC";
+	char peli2[10]="PARÁSITO";
+	char peli3[10]="ONWARD";
+	/*peliculas en cartelera*/
+	char np[10];
+	/*peli que se desea cambiar*/
+	
+	int j=0;
+	
+	setlocale(LC_ALL, "spanish");
+	/*poder escribir caracteres como la ñ y los acentos*/
+	
+	printf(" \t\t  -----CINETSIDI-----\n");
+	
+	do
+	{
+		printf("¿Qué desea hacer?:\n\t 1.INICIAR SESIÓN\t 2.CREAR UNA CUENTA\n");
+		scanf("%i", &op1);
+		/*decide si iniciar sesion o crear una cuenta*/
+		
+		if (op1!=1&&op1!=2)
+			printf("Operación no valida.\n Escriba 1 ó 2 en función de si desea iniar sesión o crear una cuenta.\n");
+		/*este bucle if es para decir al usuario que esta escribiendo mal el numero*/
+			
+	} while( op1 != 1 && op1 != 2 );
+	
 	switch (op1)
 	{
-		case 1://inicio de sesion
+		case 1:/*inicio de sesion*/
 			printf("Nombre de usuario (menos de 10 caracteres):\n");
 			scanf("%10s", nombre);
 			printf("Contraseña (menos de 10 caracteres):\n");
 			scanf("%10s", contra);
 			break;
-		case 2://crear una cuenta
+		case 2:/*crear una cuenta*/
 			printf("Nombre de usuario (menos de 10 caracteres):\n");
 			scanf("%10s", nombre);
 			printf("Contraseña (menos de 10 caracteres):\n");
@@ -56,12 +71,9 @@ double main()
 			}
 		break;
 	}
-	char peli1[10]="SONIC", peli2[10]="PARÁSITO", peli3[10]="ONWARD";//peliculas en cartelera
-	char np[10];//peli que se desea cambiar
 	printf(" \t\t -----BIENVENIDO %s-----\n \t ¿Qué operación desea realizar?\n", nombre);
 	printf(" 1.COMPRA DE ENTRADAS\n 2.CANCELACIóN DE ENTRADAS\n 3.GESTIóN DE LAS PELÍCULAS \n");
 	scanf("%i", &op2);
-	int j=0;
 	switch(op2)
 	{
 		case 1:
@@ -107,4 +119,5 @@ double main()
 			break;
 		}
 	}
+	return 0;
 }
