89c7eb88e2b29a75b4e1a86d5950df2d34dfb713
David <d.almarzag@alumnos.upm.es>
2020-03-30 15:51:08 +0200

    Update CINETSIDI 1.c

--- CINETSIDI 1.c
+++ CINETSIDI 1.c
@@ -2,67 +2,52 @@
 #include <string.h>
 #include <locale.h>
 
-int main()
+double main()
 {
 	int op1;
 	int op2;
 	int num;
-	int peli;
-	/*pelicula elegida*/
+	int peli;//pelicula elegida
 	
+	setlocale(LC_ALL, "spanish");//poder escribir caracteres como la Ò y los acentos
+	
+	printf(" \t\t  -----CINETSIDI-----\n");
+	do
+	{
+	
+	printf("øQuÈ desea hacer?:\n\t 1.INICIAR SESI”N\t 2.CREAR UNA CUENTA\n");
+	scanf("%i", &op1);//decide si iniciar sesion o crear una cuenta
+	if (op1!=1&&op1!=2) //este bucle if es para decir al usuario que esta escribiendo mal el numero
+	 {printf("OperaciÛn no valida.\n Escriba 1 Û 2 en funciÛn de si desea iniar sesiÛn o crear una cuenta.\n");
+	 }
+	}
+	while(op1!=1&&op1!=2);
 	char nombre[10];
 	char contra[10];
 	char contra1[10];
 	char contra2[10];
 	int i=0;
-	
-	char peli1[10]="SONIC";
-	char peli2[10]="PAR√ÅSITO";
-	char peli3[10]="ONWARD";
-	/*peliculas en cartelera*/
-	char np[10];
-	/*peli que se desea cambiar*/
-	
-	int j=0;
-	
-	setlocale(LC_ALL, "spanish");
-	/*poder escribir caracteres como la √± y los acentos*/
-	
-	printf(" \t\t  -----CINETSIDI-----\n");
-	
-	do
-	{
-		printf("¬øQu√© desea hacer?:\n\t 1.INICIAR SESI√ìN\t 2.CREAR UNA CUENTA\n");
-		scanf("%i", &op1);
-		/*decide si iniciar sesion o crear una cuenta*/
-		
-		if (op1!=1&&op1!=2)
-			printf("Operaci√≥n no valida.\n Escriba 1 √≥ 2 en funci√≥n de si desea iniar sesi√≥n o crear una cuenta.\n");
-		/*este bucle if es para decir al usuario que esta escribiendo mal el numero*/
-			
-	} while( op1 != 1 && op1 != 2 );
-	
 	switch (op1)
 	{
-		case 1:/*inicio de sesion*/
+		case 1://inicio de sesion
 			printf("Nombre de usuario (menos de 10 caracteres):\n");
 			scanf("%10s", nombre);
-			printf("Contrase√±a (menos de 10 caracteres):\n");
+			printf("ContraseÒa (menos de 10 caracteres):\n");
 			scanf("%10s", contra);
 			break;
-		case 2:/*crear una cuenta*/
+		case 2://crear una cuenta
 			printf("Nombre de usuario (menos de 10 caracteres):\n");
 			scanf("%10s", nombre);
-			printf("Contrase√±a (menos de 10 caracteres):\n");
+			printf("ContraseÒa (menos de 10 caracteres):\n");
 			scanf("%10s", contra1);
-			printf(" Repita la contrase√±a:\n");
+			printf(" Repita la contraseÒa:\n");
 			scanf("%10s", contra2);
 			while(strcmp(contra1,contra2)!=0)
 			{
-			printf("Las contrase√±as no coinciden, vuelva a introducirlas\n");
+			printf("Las contraseÒas no coinciden, vuelva a introducirlas\n");
 			printf("Contrase√±a (menos de 10 caracteres):\n");
 			scanf("%10s", contra1);
-			printf(" Repita la contrase√±a:\n");
+			printf(" Repita la contraseÒa:\n");
 			scanf("%10s", contra2);
 			}
 			for (i=0;i<10;i++)
@@ -71,30 +56,33 @@ int main()
 			}
 		break;
 	}
-	printf(" \t\t -----BIENVENIDO %s-----\n \t ¬øQu√© operaci√≥n desea realizar?\n", nombre);
-	printf(" 1.COMPRA DE ENTRADAS\n 2.CANCELACI√≥N DE ENTRADAS\n 3.GESTI√≥N DE LAS PEL√çCULAS \n");
+	char peli1[10]="SONIC", peli2[10]="PAR¡SITO", peli3[10]="ONWARD";//peliculas en cartelera
+	char np[10];//peli que se desea cambiar
+	printf(" \t\t -----BIENVENIDO %s-----\n \t øQuÈ operaciÛn desea realizar?\n", nombre);
+	printf(" 1.COMPRA DE ENTRADAS\n 2.CANCELACI”N DE ENTRADAS\n 3.GESTI”N DE LAS PELÕCULAS \n");
 	scanf("%i", &op2);
+	int j=0;
 	switch(op2)
 	{
 		case 1:
-		printf("¬øCu√°ntas entradas desea comprar?\n");
+		printf("øCu·ntas entradas desea comprar?\n");
 		scanf("%i", &num);
-		printf("Seleccione la pel√≠cula\n sala 1:%s\n sala 2:%s\n sala 3:%s\n", peli1, peli2, peli3);
+		printf("Seleccione la pelÌcula\n sala 1:%s\n sala 2:%s\n sala 3:%s\n", peli1, peli2, peli3);
 		scanf("%i", &peli);
 		break;
 		case 2:
-		printf("¬øCu√°ntas entradas desea devolver?\n");
+		printf("øCu·ntas entradas desea devolver?\n");
 		scanf("%i", &num);
-		printf("Seleccione la pel√≠cula\n sala 1:%s\n sala 2:%s\n sala 3:%s\n", peli1, peli2, peli3);
+		printf("Seleccione la pelÌcula\n sala 1:%s\n sala 2:%s\n sala 3:%s\n", peli1, peli2, peli3);
 		scanf("%i", &peli);
 		break;
 		case 3:
-		printf("Seleccione la pel√≠cula que desea modificar\n sala 1:%s\n sala 2:%s\n sala 3:%s\n", peli1, peli2, peli3);
+		printf("Seleccione la pelÌcula que desea modificar\n sala 1:%s\n sala 2:%s\n sala 3:%s\n", peli1, peli2, peli3);
 		scanf("%i", &peli);
 		switch (peli)
 		{
 			case 1:
-				printf("Introduce la nueva pelIcula\n");
+				printf("Introduce la nueva pelÌcula\n");
 				scanf("%s", np);
 				for (j=0;j<10;j++)
 			{
@@ -102,7 +90,7 @@ int main()
 			}
 			break;
 			case 2:
-			printf("Introduce la nueva pel√≠cula\n");
+			printf("Introduce la nueva pelÌcula\n");
 				scanf("%s", np);
 				for (j=0;j<10;j++)
 			{
@@ -110,7 +98,7 @@ int main()
 			}
 			break;
 			case 3:
-			printf("Introduce la nueva pel√≠cula\n");
+			printf("Introduce la nueva pelÌcula\n");
 				scanf("%s", np);
 				for (j=0;j<10;j++)
 			{
@@ -119,5 +107,4 @@ int main()
 			break;
 		}
 	}
-	return 0;
 }
